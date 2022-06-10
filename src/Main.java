64e5f04b950ae2525e9c890284e9f4529f8f6e04
priyank1194 <priyank9979561561@gmail.com>
2022-06-09 20:12:52 -0400

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		String x="blue pen";
+				String y="black pen";
+				String temp;
+				
+				temp=x;
+				x=y;
+				y=temp;
+		
+				System.out.println("x: "+x);
+				System.out.println("y: "+y);
+				
+		     
+	}
+
+}
